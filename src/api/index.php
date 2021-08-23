102a614214005f8b1503ba1a3518ec07ff8805b8
Rohit pai <rodude123@gmail.com>
2021-08-23 09:30:08 +0100

    created timelineData function and added it to its route

--- src/api/index.php
+++ src/api/index.php
@@ -7,17 +7,31 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
-require "routes.php";
-require "./vendor/autoload.php";
+//require “routes.php”;
+require “../vendor/autoload.php”;
+include "timelineData.php"; 
 
 // Start slim
 $app = AppFactory::create();
 // create middleware
 $app->addRoutingMiddleware();
 
-$errorMiddleware = $app->addErrorMiddleware(false, true, true);
+// for error checking
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
 
-$app->get("/timelineData", function (Request $request, Response $response)
+$app->setBasePath(“/api”);
+
+$timelineData = new TimelineData();
+
+$app->get(“/timelineData”, function (Request $request, Response $response)
 {
-
+    $result = $timelineData->getTimelineData();
+    
+    $json = json_encode($result);
+    
+    $response->getBody()->write($json);
+    
+    return $response->withHeader("Content-Type", "application/json");
 });
+
+$app->run();1q
--- src/api/timelineData.php
+++ src/api/timelineData.php
@@ -1,7 +1,25 @@
 <?php
-
+require_once "./confjg.php"
 
 class TimelineData
 {
+    function getTimelineData()
+    {
+        $conn = dbConn();
+        $stmt = $conn->prepare("SELECT * FROM timeline;"); 
+        $stmt->execute();
 
+        // set the resulting array to associative
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
+        if ($result)
+        {
+            return $result;
+        }
+        else 
+        {
+            return array("errorMessage" => "Error timeline data not found");
+        }
+          
+    }
 }
